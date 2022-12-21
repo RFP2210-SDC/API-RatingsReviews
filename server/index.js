b2ed812eafb4495465a34924d14850dc55eb65bc
Chad Fusco <fusco@case.edu>
2022-12-21 11:17:22 -0800

    progress on pg setup

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const path = require('path');
 const axios = require('axios');
 const morgan = require('morgan');
 const cors = require('cors');
-const favicon = require('serve-favicon');
-const compression = require('compression');
-const sessionHandler = require('./session-handler');
+// const favicon = require('serve-favicon');
+// const compression = require('compression');
+const ratingsReviewsAPI = require('./ratingsReviews/index.cjs');
 
 const app = express();
 
@@ -15,15 +15,12 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
-// Adds `req.session_id` based on the incoming cookie value.
-// Generates a new session if one does not exist.
-app.use(sessionHandler);
 app.use(express.urlencoded({ extended: true }));
-app.use(compression());
+// app.use(compression());
 
 // STATIC SERVICE OF ASSETS
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 // ROUTES
 app.all('/*', (req, res) => {
