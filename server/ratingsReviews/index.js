b2324e288580b46e8b68cb972913ee8b5ddc7399
Chad Fusco <fusco@case.edu>
2022-12-21 16:25:18 -0800

    minor progress

--- server/ratingsReviews/index.js
+++ server/ratingsReviews/index.js
@@ -3,15 +3,12 @@ const { Client } = require('pg');
 const client = new Client();
 client.connect();
 
-console.log('hello Chad');
 async function testing() {
-  console.log('hello Chad 2');
   const res = await client.query('SELECT $1::text as message', ['Hello world!']);
   console.log(res.rows[0].message); // Hello world!
   await client.end();
 }
 
 testing();
-console.log('hello Chad 3');
 
 module.exports = client;
