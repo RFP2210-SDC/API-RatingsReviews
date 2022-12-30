953bd630863b4ac3858b8f8fb48f66babcf6d538
Chad Fusco <fusco@case.edu>
2022-12-30 12:13:30 -0800

    progress on attempting to resolve Jest "open handle" issue

--- server/ratingsReviews/tests/server.test.js
+++ server/ratingsReviews/tests/server.test.js
@@ -1,12 +1,9 @@
 /* eslint-disable no-undef */
 // After running "npm test", open up coverage/lcov-report/index.html to see full coverage report.
 process.env.NODE_ENV = 'test';
-// const Promise = require('bluebird');
 const request = require('supertest');
 const { db } = require('../db');
 const app = require('../server');
-
-// Promise.promisifyAll(db);
 
 beforeAll(async () => {
   await db.query('DROP TABLE IF EXISTS test_characteristic_reviews');
@@ -17,16 +14,18 @@ beforeAll(async () => {
   await db.query('CREATE TABLE test_characteristics (LIKE characteristics INCLUDING ALL)');
   await db.query('CREATE TABLE test_reviews_photos (LIKE reviews_photos INCLUDING ALL)');
   await db.query('CREATE TABLE test_characteristic_reviews (LIKE characteristic_reviews INCLUDING ALL)');
-  await db.query('INSERT INTO test_characteristics (product_id, name) '
+  return db.query('INSERT INTO test_characteristics (product_id, name) '
       + "VALUES (1, 'Fit'), (1, 'Length'), (1, 'Comfort'), (1, 'Quality'), (2, 'Quality')");
-}, 30000);
+});
 
-afterAll(() => {
-  db.query('DROP TABLE IF EXISTS test_characteristic_reviews')
-    .then(() => (db.query('DROP TABLE IF EXISTS test_reviews_photos')))
-    .then(() => (db.query('DROP TABLE IF EXISTS test_characteristics')))
-    .then(() => (db.query('DROP TABLE IF EXISTS test_reviews')))
-    .finally(() => (db.end()));
+afterAll(async () => {
+  await db.query('DROP TABLE IF EXISTS test_characteristic_reviews');
+  await db.query('DROP TABLE IF EXISTS test_reviews_photos');
+  await db.query('DROP TABLE IF EXISTS test_characteristics');
+  console.log('INSIDE LAST AFTER ALL DROP');
+  await db.query('DROP TABLE IF EXISTS test_reviews');
+  console.log('INSIDE FINALLY');
+  return db.end();
 });
 
 describe('POST /reviews ', () => {
@@ -45,6 +44,7 @@ describe('POST /reviews ', () => {
           1: 1, 2: 1, 3: 1, 4: 1,
         },
       });
+    console.log('inside POST test');
     expect(response.statusCode).toBe(201);
   });
 });
