fcce480d52faa87ff5379ebfc7373dc0dffe0533
Chad Fusco <fusco@case.edu>
2022-12-31 14:19:13 -0800

    test iteration ID #1 - first k6 test setup

--- server/ratingsReviews/index.js
+++ server/ratingsReviews/index.js
@@ -4,4 +4,4 @@ const app = require('./server');
 // PORT AND SERVER LISTEN
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
-server.setTimeout(20000);
+server.setTimeout(40000);
---
+++ server/ratingsReviews/tests/k6.js
@@ -0,0 +1,62 @@
+// eslint-disable-next-line import/no-unresolved
+import http from 'k6/http';
+// eslint-disable-next-line import/no-unresolved
+import { check, sleep } from 'k6';
+
+// // EXAMPLE OF VUs
+// export const options = {
+//   vus: 1,
+//   duration: '30s',
+// };
+
+// // EXAMPLE OF VUs WITH RAMPING
+// export const options = {
+//   stages: [
+//     { duration: '30s', target: 20 },
+//     { duration: '1m30s', target: 10 },
+//     { duration: '20s', target: 0 },
+//   ],
+// };
+
+// // EXAMPLE OF VUs
+// export const options = {
+//   vus: 1,
+//   duration: '30s',
+// };
+
+export const options = {
+  scenarios: {
+    // 5 RPS - CONSTANT
+    // '5RPS': {
+    //   executor: 'constant-arrival-rate',
+    //   duration: '30s', // total duration
+    //   preAllocatedVUs: 200, // to allocate runtime resources     preAll
+    //   // rate = (desired requests rate) / (requests per iteration)
+    //   rate: 5, // number of constant iterations given `timeUnit`
+    //   timeUnit: '1s',
+    // },
+
+    // RAMPING
+    '3RPS': {
+      executor: 'ramping-arrival-rate',
+      preAllocatedVUs: 40,
+      startRate: 0,
+      timeUnit: '1s',
+      stages: [
+        { target: 3, duration: '2s' },
+        { target: 3, duration: '28s' },
+      ],
+    },
+  },
+};
+
+// TESTING SINGLE ENDPOINT - GET /reviews
+export default function () {
+  const PRODUCT_QTY = 1000011;
+  const lbID = PRODUCT_QTY * 0.9;
+  const ubID = PRODUCT_QTY;
+  const productId = Math.floor(Math.random() * (ubID - lbID) + lbID);
+  const res = http.get(`http://localhost:3000/reviews?count=2&product_id=${productId}`);
+  check(res, { 'status was 200': (r) => r.status === 200 });
+  sleep((Math.random() * (6 - 1) + 1) / 1000); // random sleep btw 1 and 6 ms
+}
